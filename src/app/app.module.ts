284f3d3c014368e4a18798b10c3d871807f51bf1
Joseg <jgracia9988@gmail.com>
2020-01-30 20:29:01 +0100

    "all works again"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
