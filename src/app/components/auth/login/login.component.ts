784cf8a6bca1a454b4fd8ec789a2a4869545bf48
Joseg <jgracia9988@gmail.com>
2020-01-31 12:58:22 +0100

    "login shows a progress bar, stepper removed"

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,28 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { FormControl, Validators } from '@angular/forms';
 
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  providers: [{
-    provide: STEPPER_GLOBAL_OPTIONS, useValue: { showError: true }
-  }]
 })
 export class LoginComponent implements OnInit {
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
+  hide = true;
+  email: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  password: FormControl = new FormControl('', [Validators.required, Validators.minLength(4)]);
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor() { }
 
   ngOnInit() {
-    this.firstFormGroup = this.formBuilder.group({
-      firstCtrl: ['', Validators.required]
-    });
-    this.secondFormGroup = this.formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
   }
+
+  getProgressBarValue(): number {
+    let progress = 100;
+    if (this.email.invalid) {
+      progress -= 50;
+    }
+    if (this.password.invalid) {
+      progress -= 50;
+    }
+    return progress;
+  }
+
+  getEmailErrorMessage(): string {
+    return this.email.hasError('required') ? 'Debes introducir un email' :
+      this.email.hasError('email') ? 'Introduce un email válido' :
+        '';
+  }
+
+  getPasswordErrorMessage(): string {
+    return this.password.hasError('required') ? 'Debes introducir una contraseña' :
+      this.password.hasError('minlength') ? 'Debes de introducir una contraseña con al menos 4 carácteres.' :
+        '';
+  }
+
 }
