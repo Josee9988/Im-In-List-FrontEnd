108d9d8d00b903379d0d63e3321e8fa143f1345a
Borja Pérez Mullor <BorjaPeMu>
2020-02-01 12:26:19 +0100

    Profile with buttons. Need value and picture from DataBase fixing

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -5,16 +5,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
+
 export class ProfileComponent implements OnInit {
 
   nickname: string;
   email: string;
   profilePicture: string;
+
+
   constructor() {
-    this.nickname = 'Carlos Alfredo'; 0
+    this.nickname = 'Carlos Alfredo';
     this.email = 'carlos98@gmail.com';
     this.profilePicture = 'https://material.angular.io/assets/img/examples/shiba1.jpg';
   }
+
 
   ngOnInit() {
   }
