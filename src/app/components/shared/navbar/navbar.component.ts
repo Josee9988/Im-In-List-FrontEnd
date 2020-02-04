165870db0cea65ed35ee27e4c74209fb35aacee2
Joseg <jgracia9988@gmail.com>
2020-02-04 18:59:40 +0100

    "navbar is hella responsive"

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -11,23 +11,24 @@ import { INavbarLinks } from './inavbar-links';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  navbarLinks: Array<INavbarLinks> = [
-    { icon: 'person', field: 'Perfil', route: 'profile' },
-    { icon: 'attach_money', field: 'Precios', route: 'pricing' },
-    { icon: 'supervised_user_circle', field: 'Sobre nosotros', route: 'about' },
-    { icon: 'contact_mail', field: 'Contacta', route: 'contact' },
-    { icon: 'post_add', field: 'Nueva lista', route: 'newList' },
-    { icon: 'fingerprint', field: 'Inicio de sesión', route: 'login' },
-    { icon: 'how_to_reg', field: 'Registro', route: 'register' }];
+  navbarLinks: Array<INavbarLinks>;
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(map(result => result.matches),
-      shareReplay()
-    );
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(
+    '(max-width: 859.99px) and (orientation: portrait), ' +
+    '(max-width: 959.99px) and (orientation: landscape)')
+    .pipe(map(result => result.matches), shareReplay());
 
   constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit() {
+    this.navbarLinks = [
+      { icon: 'person', field: 'Perfil', route: 'profile' },
+      { icon: 'attach_money', field: 'Precios', route: 'pricing' },
+      { icon: 'supervised_user_circle', field: 'Sobre nosotros', route: 'about' },
+      { icon: 'contact_mail', field: 'Contacta', route: 'contact' },
+      { icon: 'post_add', field: 'Nueva lista', route: 'newList' },
+      { icon: 'fingerprint', field: 'Inicio de sesión', route: 'login' },
+      { icon: 'how_to_reg', field: 'Registro', route: 'register' }];
   }
 
 }
