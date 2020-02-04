6bf08133a5d2f6ba861fb4ac2fb85863136c0138
Borja Pérez Mullor <BorjaPeMu>
2020-02-04 11:11:26 +0100

    Vars ready for getting values

--- src/app/components/backoffice/backoffice.component.ts
+++ src/app/components/backoffice/backoffice.component.ts
@@ -9,23 +9,76 @@ import { MultiDataSet, Label } from 'ng2-charts';
 })
 
 export class BackofficeComponent {
-  nickname: string;
-  email: string;
-  profilePicture: string;
+  registradosEnero: number;
+  registradosFebrero: number;
+  registradosMarzo: number;
+  registradosAbril: number;
+  registradosMayo: number;
+  registradosJunio: number;
+  registradosJulio: number;
+  registradosAgosto: number;
+  registradosSeptiembre: number;
+  registradosOctubre: number;
+  registradosNoviembre: number;
+  registradosDiciembre: number;
+  premiumEnero: number;
+  premiumFebrero: number;
+  premiumMarzo: number;
+  premiumAbril: number;
+  premiumMayo: number;
+  premiumJunio: number;
+  premiumJulio: number;
+  premiumAgosto: number;
+  premiumSeptiembre: number;
+  premiumOctubre: number;
+  premiumNoviembre: number;
+  premiumDiciembre: number;
+  listasEnero: number;
+  listasFebrero: number;
+  listasMarzo: number;
+  listasAbril: number;
+  listasMayo: number;
+  listasJunio: number;
+  listasJulio: number;
+  listasAgosto: number;
+  listasSeptiembre: number;
+  listasOctubre: number;
+  listasNoviembre: number;
+  listasDiciembre: number;
+
+
+
 
   constructor() {
-
+    this.premiumEnero = 154;
+    this.premiumAbril = 1;
+    this.premiumMayo = 1;
   }
 
-  //Clases donde se almacenerán los valores
+  // Clases donde se almacenerán los valores
   public doughnutChartLabels: Label[] = ['Enero', 'Febrero', 'Marzo',
     'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
-  //Valores obtenidos de la base de datos
-  public doughnutChartData: MultiDataSet = [
-    [350, 403, 200, 10, 40, 512],
-
+  // Valores obtenidos de la base de datos para usuarios registrados
+  public doughnutChartRegisterUsers: MultiDataSet = [
+    [this.registradosEnero, this.registradosFebrero, this.registradosMarzo, this.registradosAbril,
+    this.registradosMayo, this.registradosJunio, this.registradosJulio, this.registradosAgosto,
+    this.registradosSeptiembre, this.registradosOctubre, this.registradosNoviembre, this.registradosDiciembre],
   ];
+  // Valores obtenidos de la base de datos para usuarios premium
+  public doughnutChartPremiumUsers: MultiDataSet = [
+    [this.premiumEnero, this.premiumFebrero, this.premiumMarzo, this.premiumAbril,
+    this.premiumMayo, this.premiumJunio, this.premiumJulio, this.premiumAgosto,
+    this.premiumSeptiembre, this.premiumOctubre, this.premiumNoviembre, this.premiumDiciembre],
+  ];
+
+  // Valores obtenidos de la base de datos para listas creadas
+  public doughnutChartCreatedLists: MultiDataSet = [
+    [this.listasEnero, this.listasFebrero, this.listasMarzo, this.listasAbril,
+    this.listasMayo, this.listasJunio, this.listasJulio, this.listasAgosto,
+    this.listasSeptiembre, this.listasOctubre, this.listasNoviembre, this.listasDiciembre],
+  ];
+
   public doughnutChartType: ChartType = 'doughnut';
 
 
