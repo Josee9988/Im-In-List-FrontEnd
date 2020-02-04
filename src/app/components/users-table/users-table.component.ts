574eae79f99257a951ec720f84b1d52eed477b8a
borja <multibalcoy@gmail.com>
2020-02-04 13:06:34 +0100

    Table with example users

--- src/app/components/users-table/users-table.component.ts
+++ src/app/components/users-table/users-table.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { getMaxListeners } from 'cluster';
+
 
 /**
  * @title Table with pagination
@@ -13,7 +13,7 @@ import { getMaxListeners } from 'cluster';
 
 })
 export class UsersTableComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'nombre', 'email', 'rol'];
+  displayedColumns: string[] = ['id', 'nombre', 'email', 'rol', 'listasCreadas', 'listasParticipante'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
@@ -28,27 +28,89 @@ export interface PeriodicElement {
   id: number;
   email: string;
   rol: string;
+  listasCreadas: Array<number>;
+  listasParticipante: Array<number>;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { id: 1, nombre: 'Hydrogen', email: 'carlosAlfredo@getMaxListeners.com', rol: 'H' },
-  { id: 2, nombre: 'Helium', email: 'carlosAlfredo@getMaxListeners.com', rol: 'He' },
-  { id: 3, nombre: 'Lithium', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Li' },
-  { id: 4, nombre: 'Beryllium', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Be' },
-  { id: 5, nombre: 'Boron', email: 'carlosAlfredo@getMaxListeners.com', rol: 'B' },
-  { id: 6, nombre: 'Carbon', email: 'carlosAlfredo@getMaxListeners.com', rol: 'C' },
-  { id: 7, nombre: 'Nitrogen', email: 'carlosAlfredo@getMaxListeners.com', rol: 'N' },
-  { id: 8, nombre: 'Oxygen', email: 'carlosAlfredo@getMaxListeners.com', rol: 'O' },
-  { id: 9, nombre: 'Fluorine', email: 'carlosAlfredo@getMaxListeners.com', rol: 'F' },
-  { id: 10, nombre: 'Neon', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Ne' },
-  { id: 11, nombre: 'Sodium', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Na' },
-  { id: 12, nombre: 'Magnesium', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Mg' },
-  { id: 13, nombre: 'Aluminum', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Al' },
-  { id: 14, nombre: 'Silicon', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Si' },
-  { id: 15, nombre: 'Phosphorus', email: 'carlosAlfredo@getMaxListeners.com', rol: 'P' },
-  { id: 16, nombre: 'Sulfur', email: 'carlosAlfredo@getMaxListeners.com', rol: 'S' },
-  { id: 17, nombre: 'Chlorine', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Cl' },
-  { id: 18, nombre: 'Argon', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Ar' },
-  { id: 19, nombre: 'Potassium', email: 'carlosAlfredo@getMaxListeners.com', rol: 'K' },
-  { id: 20, nombre: 'Calcium', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Ca' },
+  {
+    id: 1, nombre: 'Hydrogen', email: 'carlosAlfredo@getMaxListeners.com', rol: '0',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 2, nombre: 'Helium', email: 'carlosAlfredo@getMaxListeners.com', rol: '1',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 3, nombre: 'Lithium', email: 'carlosAlfredo@getMaxListeners.com', rol: '1',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 4, nombre: 'Beryllium', email: 'carlosAlfredo@getMaxListeners.com', rol: '1',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 5, nombre: 'Boron', email: 'carlosAlfredo@getMaxListeners.com', rol: '1',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 6, nombre: 'Carbon', email: 'carlosAlfredo@getMaxListeners.com', rol: '2',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 7, nombre: 'Nitrogen', email: 'carlosAlfredo@getMaxListeners.com', rol: '1',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 8, nombre: 'Oxygen', email: 'carlosAlfredo@getMaxListeners.com', rol: '1',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 9, nombre: 'Fluorine', email: 'carlosAlfredo@getMaxListeners.com', rol: '2',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 10, nombre: 'Neon', email: 'carlosAlfredo@getMaxListeners.com', rol: '1',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 11, nombre: 'Sodium', email: 'carlosAlfredo@getMaxListeners.com', rol: '2',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 12, nombre: 'Magnesium', email: 'carlosAlfredo@getMaxListeners.com', rol: '2',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 13, nombre: 'Aluminum', email: 'carlosAlfredo@getMaxListeners.com', rol: '1',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 14, nombre: 'Silicon', email: 'carlosAlfredo@getMaxListeners.com', rol: '2',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 15, nombre: 'Phosphorus', email: 'carlosAlfredo@getMaxListeners.com', rol: '0',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 16, nombre: 'Sulfur', email: 'carlosAlfredo@getMaxListeners.com', rol: '1',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 17, nombre: 'Chlorine', email: 'carlosAlfredo@getMaxListeners.com', rol: '2',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 18, nombre: 'Argon', email: 'carlosAlfredo@getMaxListeners.com', rol: '1',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 19, nombre: 'Potassium', email: 'carlosAlfredo@getMaxListeners.com', rol: '2',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
+  {
+    id: 20, nombre: 'Calcium', email: 'carlosAlfredo@getMaxListeners.com', rol: '1',
+    listasCreadas: [1, 2, 3], listasParticipante: [1, 2, 3]
+  },
 ];
