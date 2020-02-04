b9e3e35e7396c24bf85ca4a4485d58e610fd8969
Borja Pérez Mullor <BorjaPeMu>
2020-02-04 12:05:56 +0100

    Added link from backoffice to admin tables

--- src/app/components/users-table/users-table.component.ts
+++ src/app/components/users-table/users-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { getMaxListeners } from 'cluster';
 
 /**
  * @title Table with pagination
@@ -12,7 +13,7 @@ import { MatTableDataSource } from '@angular/material/table';
 
 })
 export class UsersTableComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['id', 'nombre', 'email', 'rol'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
@@ -23,31 +24,31 @@ export class UsersTableComponent implements OnInit {
 }
 
 export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  nombre: string;
+  id: number;
+  email: string;
+  rol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
+  { id: 1, nombre: 'Hydrogen', email: 'carlosAlfredo@getMaxListeners.com', rol: 'H' },
+  { id: 2, nombre: 'Helium', email: 'carlosAlfredo@getMaxListeners.com', rol: 'He' },
+  { id: 3, nombre: 'Lithium', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Li' },
+  { id: 4, nombre: 'Beryllium', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Be' },
+  { id: 5, nombre: 'Boron', email: 'carlosAlfredo@getMaxListeners.com', rol: 'B' },
+  { id: 6, nombre: 'Carbon', email: 'carlosAlfredo@getMaxListeners.com', rol: 'C' },
+  { id: 7, nombre: 'Nitrogen', email: 'carlosAlfredo@getMaxListeners.com', rol: 'N' },
+  { id: 8, nombre: 'Oxygen', email: 'carlosAlfredo@getMaxListeners.com', rol: 'O' },
+  { id: 9, nombre: 'Fluorine', email: 'carlosAlfredo@getMaxListeners.com', rol: 'F' },
+  { id: 10, nombre: 'Neon', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Ne' },
+  { id: 11, nombre: 'Sodium', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Na' },
+  { id: 12, nombre: 'Magnesium', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Mg' },
+  { id: 13, nombre: 'Aluminum', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Al' },
+  { id: 14, nombre: 'Silicon', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Si' },
+  { id: 15, nombre: 'Phosphorus', email: 'carlosAlfredo@getMaxListeners.com', rol: 'P' },
+  { id: 16, nombre: 'Sulfur', email: 'carlosAlfredo@getMaxListeners.com', rol: 'S' },
+  { id: 17, nombre: 'Chlorine', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Cl' },
+  { id: 18, nombre: 'Argon', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Ar' },
+  { id: 19, nombre: 'Potassium', email: 'carlosAlfredo@getMaxListeners.com', rol: 'K' },
+  { id: 20, nombre: 'Calcium', email: 'carlosAlfredo@getMaxListeners.com', rol: 'Ca' },
 ];
