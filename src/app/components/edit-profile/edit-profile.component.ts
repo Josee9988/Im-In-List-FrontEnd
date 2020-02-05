a0fb874ab6ee1fdff555b3f3f7446837736a3fc7
borja <multibalcoy@gmail.com>
2020-02-05 20:35:31 +0100

    Methods and calls to SUPER(), extends from login/register component

--- src/app/components/edit-profile/edit-profile.component.ts
+++ src/app/components/edit-profile/edit-profile.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginRegisterComponent } from '../auth/login-register/login-register.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
   styleUrls: ['./edit-profile.component.scss']
 })
-export class EditProfileComponent implements OnInit {
+export class EditProfileComponent extends LoginRegisterComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(router: Router) {
+    super(router);
+  }
 
   ngOnInit() {
   }
