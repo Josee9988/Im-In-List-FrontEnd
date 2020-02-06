4362f2828eaac9b903f4d7e1d76171868723dada
borja <multibalcoy@gmail.com>
2020-02-06 18:07:38 +0100

    Check pass method

--- src/app/components/edit-profile/edit-profile.component.ts
+++ src/app/components/edit-profile/edit-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
 
 
@@ -13,64 +13,88 @@ export class EditProfileComponent implements OnInit {
   name: FormControl;
   email: FormControl;
   password: FormControl;
+  confirmPassword: FormControl;
   inputs: Array<FormControl>;
   files: File[];
+  groupPassword: FormGroup;
 
   constructor(private location: Location) {
     this.hide = true;
     this.email = new FormControl('', [Validators.required, Validators.email, Validators.maxLength(255)]);
     this.password = new FormControl('', [Validators.required, Validators.minLength(4)]);
+    this.confirmPassword = new FormControl('', [Validators.required]);
     this.files = [];
 
     this.name = new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(60)]);
     this.inputs = [this.email, this.password, this.name];
 
+
   }
 
-  ngOnInit() {
+  checkPasswords(groupPassword: FormGroup) { // here we have the 'passwords' group
+    const password = groupPassword.get('password').value;
+    const confirmPassword = groupPassword.get('password').value;
+
+    return password === confirmPassword ? null : { notSame: true };
   }
 
-  getProgressBarValue(): number {
-    let progress = 100;
-    for (const input of this.inputs) {
-      if (input.invalid) {
-        progress -= 100 / this.inputs.length;
-      }
+
+  this.myForm = this.fb.group({
+    password: ['', [Validators.required]],
+    confirmPassword: ['']
+  }, { validator: this.checkPasswords })
+
+
+ngOnInit() {
+}
+
+getProgressBarValue(): number {
+  let progress = 100;
+  for (const input of this.inputs) {
+    if (input.invalid) {
+      progress -= 100 / this.inputs.length;
     }
-    return progress;
   }
+  return progress;
+}
 
-  getEmailErrorMessage(): string {
-    return this.email.hasError('required') ? 'Debes introducir un email' :
-      this.email.hasError('email') ? 'Introduce un email válido' :
-        this.email.hasError('maxLength') ? 'Debes de introducir un email con menos de 255 carácteres.' :
-          '';
-  }
-
-  getPasswordErrorMessage(): string {
-    return this.password.hasError('required') ? 'Debes introducir una contraseña' :
-      this.password.hasError('minlength') ? 'Debes de introducir una contraseña con al menos 4 carácteres.' :
+getEmailErrorMessage(): string {
+  return this.email.hasError('required') ? 'Debes introducir un email' :
+    this.email.hasError('email') ? 'Introduce un email válido' :
+      this.email.hasError('maxLength') ? 'Debes de introducir un email con menos de 255 carácteres.' :
         '';
-  }
+}
 
-  getNameErrorMessage(): string {
-    return this.name.hasError('required') ? 'Debes introducir un nombre de usuario' :
-      this.name.hasError('minlength') ? 'Debes de introducir un nombre de usuario con al menos 4 carácteres.' :
-        this.email.hasError('maxLength') ? 'Debes de introducir un nombre de usuario con menos de 60 carácteres.' :
-          '';
-  }
+getPasswordErrorMessage(): string {
+  return this.password.hasError('required') ? 'Debes introducir una contraseña' :
+    this.password.hasError('minlength') ? 'Debes de introducir una contraseña con al menos 4 carácteres.' :
+      '';
+}
 
-  onSelect(event) {
-    console.log(event);
-    this.files.push(...event.addedFiles);
-  }
+getConfirmPasswordErrorMessage(): string {
+  return this.confirmPassword.hasError('required') ? 'Debes introducir la confirmación de contraseña' :
+    this.confirmPassword.hasError('must') ? 'Debes de introducir una contraseña con al menos 4 carácteres.' :
+      '';
+}
+getNameErrorMessage(): string {
+  return this.name.hasError('required') ? 'Debes introducir un nombre de usuario' :
+    this.name.hasError('minlength') ? 'Debes de introducir un nombre de usuario con al menos 4 carácteres.' :
+      this.email.hasError('maxLength') ? 'Debes de introducir un nombre de usuario con menos de 60 carácteres.' :
+        '';
+}
 
-  onRemove(event) {
-    console.log(event);
-    this.files.splice(this.files.indexOf(event), 1);
-  }
 
-  goBack(): void {
-    this.location.back();
-  }
+onSelect(event) {
+  console.log(event);
+  this.files.push(...event.addedFiles);
+}
+
+onRemove(event) {
+  console.log(event);
+  this.files.splice(this.files.indexOf(event), 1);
+}
+
+goBack(): void {
+  this.location.back();
+}
 }
