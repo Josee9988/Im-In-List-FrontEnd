48b29b5bcebbba35d431e8a5d65cbaffdf4e214c
Joseg <jgracia9988@gmail.com>
2020-02-11 17:01:33 +0100

    "auth service improved and now can check if the token exists"

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -8,14 +8,21 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   /**
+   * The name of the local storage item that will be saving the token.
+   */
+  private storageName: string;
+
+  constructor() { this.storageName = 'loginUserToken'; }
+
+  /**
    * Gets the authorization token from the local storage, if so it will return the token,
    * otherwise it will return null.
    *
    * @returns string | null : String if the token was found, otherwise null.
    */
   getAuthorizationToken(): string | null {
-    if (localStorage.getItem('loginUserToken')) {
-      return localStorage.getItem('loginUserToken');
+    if (this.hasToken) {
+      return localStorage.getItem(this.storageName);
     }
     return null;
   }
@@ -24,9 +31,23 @@ export class AuthService {
    * Summary: removes the token from the localStorage.
    */
   deleteAuthorizationToken(): void {
-    if (localStorage.getItem('loginUserToken')) {
-      localStorage.removeItem('loginUserToken');
+    if (this.hasToken) {
+      localStorage.removeItem(this.storageName);
     }
     return null;
+  }
+
+  /**
+   * Summary: checks if the token exists and if so  returns true, otherwise false.
+   *
+   * @return boolean, true if the token exists otherwise false.
+   */
+  hasToken(): boolean {
+    const lStorageItem: string = localStorage.getItem(this.storageName);
+    if (lStorageItem !== null && typeof lStorageItem === 'string' && lStorageItem.length > 300) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
