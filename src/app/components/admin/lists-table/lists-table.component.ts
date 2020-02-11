82d619071dcdfde9f826dd760b15a5e3f7146cd1
borja <multibalcoy@gmail.com>
2020-02-11 13:48:13 +0100

    Added the documentation for the functions of listTable component

--- src/app/components/admin/lists-table/lists-table.component.ts
+++ src/app/components/admin/lists-table/lists-table.component.ts
@@ -10,11 +10,14 @@ import { ListaService } from './../../../shared/services/lista.service';
   styleUrls: ['./lists-table.component.scss'],
 
 })
+/**
+ * @author Borja Pérez Mullor <multibalcoy@gmail.com>
+ */
 export class ListsTableComponent implements OnInit {
 
   items: any;
   displayedColumns: string[] = ['id', 'idCreador', 'titulo', 'descripcion', 'elemento', 'acciones'];
-  dataSource = new MatTableDataSource<Element>();
+  dataSource = new MatTableDataSource<IlistaDetails>();
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
@@ -32,18 +35,25 @@ export class ListsTableComponent implements OnInit {
     this.dataSource.sort = this.sort;
   }
 
+  /**
+   * Sumary: This function receive a string and filter the results which one contains that string
+   * @param filterValue Is what user introduceson the input and filter the data
+   */
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-
+  /**
+   * Sumary: This function is used to fill data inside dataSource for show it on table
+   * @param Response is the data that has been received from database
+   */
   fillDataLists(Response: any) {
     this.items = Response;
     this.dataSource.data = this.items;
 
   }
 }
-/*
+
 export interface IlistaDetails {
   id: number;
   idCreador: number;
@@ -52,6 +62,6 @@ export interface IlistaDetails {
   elemento: number;
   acciones: string;
 }
-
+/*
 const dataLista: IlistaDetails[] = [];
 */
