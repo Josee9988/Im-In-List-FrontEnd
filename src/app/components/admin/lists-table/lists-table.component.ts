a5d4823db736e2a5e7024ceae3f6a7b20faece44
borja <multibalcoy@gmail.com>
2020-02-11 12:36:44 +0100

    Imported listaService

--- src/app/components/admin/lists-table/lists-table.component.ts
+++ src/app/components/admin/lists-table/lists-table.component.ts
@@ -2,10 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
+import { ListaService } from './../../../shared/services/lista.service';
 
-/**
- * @title Table with pagination
- */
 @Component({
   selector: 'app-lists-able',
   templateUrl: './lists-table.component.html',
@@ -19,13 +17,28 @@ export class ListsTableComponent implements OnInit {
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
+  constructor(private listaService: ListaService) {
+
+  }
+
+
   ngOnInit() {
+    // Llamamos a la funcion que asignará todos los valores a sus variables
+    this.listaService.getListas().subscribe(Response => this.fillDataLists(Response));
+
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+
+  fillDataLists(Response: any) {
+    for (const lista of Response) {
+
+    }
   }
 }
 
