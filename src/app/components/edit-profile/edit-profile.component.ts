4522f9c9483dad261147fb722e5b2e32c936b2f2
borja <multibalcoy@gmail.com>
2020-02-13 17:42:57 +0100

    Added documentation to the functions

--- src/app/components/edit-profile/edit-profile.component.ts
+++ src/app/components/edit-profile/edit-profile.component.ts
@@ -57,7 +57,9 @@ export class EditProfileComponent extends Forms {
     if (this.editName === true) {
       this.inputs = [this.name];
     } else if (this.editEmail === true) {
-
+      this.inputs = [this.email];
+    } else if (this.editPassword === true) {
+      this.inputs = [this.oldPassword, this.password, this.confirmPassword];
     }
   }
 
@@ -94,6 +96,9 @@ export class EditProfileComponent extends Forms {
 
   }
 
+  /**
+   * Sumary: This function check if the password is the same as confirmed one
+   */
   checkPasswords(): boolean {
     if (this.password.value === this.confirmPassword.value) {
       return true;
@@ -101,19 +106,29 @@ export class EditProfileComponent extends Forms {
     return false;
   }
 
+  /**
+   * Sumary: If there is any error on the input, this function will return a message.
+   * @return string with errors found
+   */
   getEmailErrorMessage(): string {
     return this.email.hasError('required') ? 'Debes introducir un email' :
       this.email.hasError('email') ? 'Introduce un email válido' :
         this.email.hasError('maxLength') ? 'Debes de introducir un email con menos de 255 carácteres.' :
           '';
   }
-
+  /**
+   * Sumary: If there is any error on the input, this function will return a message.
+   * @return string with errors found
+   */
   getPasswordErrorMessage(): string {
     return this.password.hasError('required') ? 'Debes introducir una contraseña' :
       this.password.hasError('minlength') ? 'Debes de introducir una contraseña con al menos 4 carácteres.' :
         '';
   }
-
+  /**
+   * Sumary: If there is any error on the input, this function will return a message.
+   * @return string with errors found
+   */
   getConfirmPasswordErrorMessage(): string {
     if (this.checkPasswords()) {
       return this.confirmPassword.hasError('required') ? 'Debes introducir la confirmación de contraseña' :
@@ -125,10 +140,12 @@ export class EditProfileComponent extends Forms {
       return 'Las contraseñas nuevas deben coincidir. ' + toReturn;
 
     }
-
-
-
   }
+
+  /**
+   * Sumary: If there is any error on the input, this function will return a message.
+   * @return string with errors found
+   */
   getNameErrorMessage(): string {
     return this.name.hasError('required') ? 'Debes introducir un nombre de usuario' :
       this.name.hasError('minlength') ? 'Debes de introducir un nombre de usuario con al menos 4 carácteres.' :
@@ -136,15 +153,25 @@ export class EditProfileComponent extends Forms {
           '';
   }
 
-
-  onSelect(event) {
-    this.files.push(...event.addedFiles);
+  /**
+   * Sumary: This function push the picture that user have choosed to the array of files
+   * @param picture Is the picture that user add
+   */
+  onSelect(picture: any) {
+    this.files.push(...picture.addedFiles);
   }
 
-  onRemove(event) {
-    this.files.splice(this.files.indexOf(event), 1);
+  /**
+   * Sumary: This function remove the picture that user have choosed to the array of files
+   * @param picture Is the picture that user decide to remove
+   */
+  onRemove(picture: any) {
+    this.files.splice(this.files.indexOf(picture), 1);
   }
 
+  /**
+   * Sumary: This function will redirect the user back
+   */
   goBack(): void {
     this.location.back();
   }
