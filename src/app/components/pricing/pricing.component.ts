cacd7c5073b092b06996d7b1b9ccc112545ba471
Joseg <jgracia9988@gmail.com>
2020-02-14 18:13:06 +0100

    "implemented ngondestroy to destroy the paypal script tag"

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewChecked, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { Component, AfterViewChecked, ElementRef, ViewChild, OnInit, OnDestroy } from '@angular/core';
 import { SnackBarErrorType } from 'src/app/shared/enums/snackbar-error-type.enum';
 import { SnackbarDisplayerService } from 'src/app/shared/services/snackbar-displayer.service';
 import { AuthService } from 'src/app/shared/services/auth.service';
@@ -10,7 +10,7 @@ declare let paypal: any;
   templateUrl: './pricing.component.html',
   styleUrls: ['./pricing.component.scss']
 })
-export class PricingComponent implements AfterViewChecked, OnInit {
+export class PricingComponent implements AfterViewChecked, OnInit, OnDestroy {
   addScript: boolean;
   paypalLoad: boolean;
   finalAmount: number;
@@ -50,18 +50,6 @@ export class PricingComponent implements AfterViewChecked, OnInit {
     };
   }
 
-
-  ngAfterViewChecked(): void {
-    if (!this.addScript) {
-      this.addPaypalScript().then(() => {
-        paypal.Button.render(this.paypalConfig, '#paypal-checkout-btn');
-        this.paypalLoad = false;
-      }).catch((error) => {
-        this.snackbarDisplayer.openSnackBar('Error al cargar conectar con PayPal: ' + error, SnackBarErrorType.warning);
-      });
-    }
-  }
-
   ngOnInit(): void {
     if (this.authService.hasToken()) { // si sí tiene el token
       this.userService.getDataUser().subscribe(Response => {
@@ -78,6 +66,28 @@ export class PricingComponent implements AfterViewChecked, OnInit {
     }
   }
 
+  ngAfterViewChecked(): void {
+    if (!this.addScript) {
+      this.addPaypalScript().then(() => {
+        paypal.Button.render(this.paypalConfig, '#paypal-checkout-btn');
+        this.paypalLoad = false;
+      }).catch((error) => {
+        this.snackbarDisplayer.openSnackBar('Error al cargar conectar con PayPal: ' + error, SnackBarErrorType.warning);
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (document.getElementById('paypalsrctag') ? true : false) {
+      const srcTag = document.getElementById('paypalsrctag');
+      if (srcTag.parentNode) {
+        srcTag.parentNode.removeChild(srcTag);
+      } else {
+        srcTag.remove();
+      }
+    }
+  }
+
   /**
    * Summary: function that is invoked after the view is checked and after we add
    * the PayPal source code for making the calls.
@@ -87,15 +97,12 @@ export class PricingComponent implements AfterViewChecked, OnInit {
   addPaypalScript(): Promise<any> {
     this.addScript = true;
     return new Promise((resolve) => {
-
       if (!document.getElementById('paypalsrctag') ? true : false) { // if false the script is not already loaded and it will do it
         const scripttagElement = document.createElement('script');
         scripttagElement.id = 'paypalsrctag';
         scripttagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
         scripttagElement.onload = resolve;
         document.body.appendChild(scripttagElement);
-        console.log('added');
-
       }
     });
   }
