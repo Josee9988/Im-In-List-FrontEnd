4510d3c637dadd15baa211e5186329727cdbfeea
Joseg <jgracia9988@gmail.com>
2020-02-15 16:01:41 +0100

    "it shows the created list, a copy to clipboard element and a QR code"

--- src/app/components/list/show-dialog/show-dialog.component.ts
+++ src/app/components/list/show-dialog/show-dialog.component.ts
@@ -1,21 +1,41 @@
-import { Component, Inject, Injectable } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
+import { environment } from './../../../../environments/environment';
 
 interface IDialogUrl {
   url: string;
 }
-@Injectable({
-  providedIn: 'root'
-})
+
 @Component({
   selector: 'app-show-dialog',
   templateUrl: './show-dialog.component.html',
   styleUrls: ['./show-dialog.component.scss']
 })
+/**
+ * @author Jose Gracia Berenguer <jgracia9988@gmail.com>
+ */
 export class ShowDialogComponent {
+  private readonly SITE_URL: string = environment.siteURl;
   public url: string;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: IDialogUrl) {
-    this.url = data.url;
+    this.url = `${this.SITE_URL}list/${data.url}`;
+  }
+
+  /**
+   * Summary: Creates a HTML element, sets the value to the URL that we want to be copied,
+   * and executes the copy command, so the user will have the URL copied in his clipboard,
+   * after the text is copied it removes the created element.
+   */
+  copyUrl(): void {
+    const selBox = document.createElement('textarea');
+    selBox.style.opacity = '0';
+    selBox.style.display = 'none';
+    selBox.value = this.url;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
   }
 }
