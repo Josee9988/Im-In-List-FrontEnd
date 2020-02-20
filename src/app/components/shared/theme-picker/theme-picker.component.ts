abbaad960c22fad641606e7e17d1b4cf847b493e
Joseg <jgracia9988@gmail.com>
2020-02-20 18:32:30 +0100

    "picker prepared"

--- src/app/components/shared/theme-picker/theme-picker.component.ts
+++ src/app/components/shared/theme-picker/theme-picker.component.ts
@@ -5,11 +5,40 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './theme-picker.component.html',
   styleUrls: ['./theme-picker.component.scss']
 })
-export class ThemePickerComponent implements OnInit {
+export class ThemePickerComponent {
+  currentTheme;
 
-  constructor() { }
+  themes = [
+    {
+      primary: '#673AB7',
+      accent: '#FFC107',
+      href: 'deeppurple-amber.css',
+      isDark: false,
+    },
+    {
+      primary: '#3F51B5',
+      accent: '#E91E63',
+      href: 'indigo-pink.css',
+      isDark: false,
+      isDefault: true,
+    },
+    {
+      primary: '#E91E63',
+      accent: '#607D8B',
+      href: 'pink-bluegrey.css',
+      isDark: true,
+    },
+    {
+      primary: '#9C27B0',
+      accent: '#4CAF50',
+      href: 'purple-green.css',
+      isDark: true,
+    },
+  ];
 
-  ngOnInit() {
+  constructor(
+  ) {
+
   }
 
 }
