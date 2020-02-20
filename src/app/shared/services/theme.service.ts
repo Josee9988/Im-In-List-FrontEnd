2ff912576f6aeae911f5f7345a5b4de682d6c3c9
Joseg <jgracia9988@gmail.com>
2020-02-20 20:00:48 +0100

    "theme service created, and called in the logout"

---
+++ src/app/shared/services/theme.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+/**
+ * @author Jose Gracia Berenguer <jgracia9988@gmail.com>
+ */
+export class ThemeService {
+  /**
+   * The name of the local storage item that will be saving the theme id..
+   */
+  private readonly storageName: string = 'selectedTheme';
+
+  /**
+   * Summary: returns the actual theme id.
+   *
+   * @returns number | null : Number if the token was found, otherwise null.
+   */
+  getActualTheme(): number | null {
+    const lStorageItem: string = localStorage.getItem(this.storageName);
+    if (lStorageItem) {
+      return Number(lStorageItem);
+    }
+    return null;
+  }
+
+  /**
+   * Summary: saves the theme id received.
+   *
+   * @param number the theme id.
+   */
+  saveTheme(id: number): void {
+    this.deleteTheme();
+    localStorage.setItem(this.storageName, id.toString());
+  }
+
+  /**
+   * Summaru: deletes the theme id.
+   */
+  deleteTheme(): void {
+    localStorage.removeItem(this.storageName);
+  }
+}
