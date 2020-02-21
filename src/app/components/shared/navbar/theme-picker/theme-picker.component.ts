638abd23de92b5e4a6eadec1d3972386890ffdc0
Joseg <jgracia9988@gmail.com>
2020-02-21 15:46:07 +0100

    "improvements"

--- src/app/components/shared/navbar/theme-picker/theme-picker.component.ts
+++ src/app/components/shared/navbar/theme-picker/theme-picker.component.ts
@@ -9,37 +9,34 @@ import { ITheme } from './ITheme.interface';
 })
 export class ThemePickerComponent implements OnInit {
   currentTheme: number;
-  themes: Array<ITheme> = [
-    {
-      primary: '#673ab7', // default
-      id: 0
-    },
-    {
-      primary: '#1a1717', // dark theme
-      id: 1
-    },
-    {
-      primary: '#e91e63', // red theme
-      id: 2
-    },
-    {
-      primary: '#3f51b5', // blue theme
-      id: 3
-    },
-  ];
+  themes: Array<ITheme>;
 
   constructor(private themeService: ThemeService) { }
 
   ngOnInit(): void {
+    // theme initialization
+    this.themes = [
+      { primary: '#673ab7', id: 0 }, // default
+      { primary: '#1a1717', id: 1 }, // dark theme
+      { primary: '#e91e63', id: 2 }, // red theme
+      { primary: '#3f51b5', id: 3 }, // blue theme
+    ];
+
     if (this.themeService.getActualTheme() !== null) {
       this.currentTheme = this.themeService.getActualTheme();
       this.onInstallTheme(this.currentTheme);
     } else {
       this.currentTheme = 0;
     }
-
   }
 
+  /**
+   * Summary: it receives an id of a theme and installs it. It also saves the id of the theme
+   * in the local storage
+   * 
+   *
+   * @param id the id of the theme.
+   */
   onInstallTheme(id: number) {
     this.themeService.saveTheme(id);
     this.currentTheme = id;
